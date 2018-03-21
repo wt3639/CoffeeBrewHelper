b0880bea833317cebebab9595fe77bf06e787c0e
wt3639 <wutao3639@163.com>
2018-03-21 18:06:53 +0800

    fix timer

--- pages/brew/brew.js
+++ pages/brew/brew.js
@@ -1,31 +1,32 @@
 const app = getApp();
 var timer
 var second = 0;
-var countSecond;
+var partSecond=0;
+var brewTime;
 var totalSecond = 0;
-var totalCountSecond = 0;
+var totalTime = 0;
+var partTime=0;
+var preStopBrew = false;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    brewPercent: 0,
+    partPercent: 0,
     second: 0,
-    totalPercent: 0,
-    totalSecond: 0,
+    partSecond:0,
     startHide:false,
-    stopHide:true,
-    brewPercentHide:false,
-    totalPercentHide:true,
+    stopBrewHide:true,
+    stopHide:true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    countSecond = parseInt(options.brewTime.split(":")[0])*60 + parseInt(options.brewTime.split(":")[1]);
-    totalCountSecond = parseInt(options.totalTime.split(":")[0])*60+ parseInt(options.totalTime.split(":")[1]);
+    brewTime = parseInt(options.brewTime.split(":")[0])*60 + parseInt(options.brewTime.split(":")[1]);
+    totalTime = parseInt(options.totalTime.split(":")[0])*60+ parseInt(options.totalTime.split(":")[1]);
   },
 
   /**
@@ -84,46 +85,66 @@ Page({
     this.setData({
       second: 0,
       brewPercent: 0,
+      brewSecond:0,
       totalSecond: 0,
       totalPercent: 0,
       startHide:true,
-      stopHide:false,
+      stopBrewHide:false,
     })
     second = 0;
+    partSecond =0;
     totalSecond = 0;
+    partTime = brewTime;
     Countdown(this);
   },
 
-  stopTimer:function(){
+  stopBrewTimer:function(){
     console.log("完成焖蒸按钮");
-    clearTimeout(timer);
     this.setData({
-      totalSecond: 0,
-      totalPercent: 0,
-      startHide: false,
-      stopHide: true,
-    })
-    totalSecond = 0;
-    Countdown2(this);
+      partPercent: 0,
+      partSecond:0,
+      startHide: true,
+      stopBrewHide: true,
+      stopHide:false,
+    });
+    partSecond = 1;
+    partTime = totalTime - brewTime;
+    preStopBrew = true;
+    
+  },
+  
+  stopTimer:function(){
+    console.log("完成注水按钮");
+    clearTimeout(timer);
+
   }
 
 })
 
 function Countdown(that) {
-  if (second == countSecond) {
-    console.log("Time up...");
+  if (second == brewTime && preStopBrew==false) {
+    console.log("Brew Times up...");
     that.setData({
-      brewPercent: 100
+      partPercent: 0,
+      stopBrewHide: true,
+      stopHide: false,
     });
-    Countdown2(that);
-    return;
+    partSecond=0;
+    partTime = totalTime - brewTime;
   }
+  
+  if(second == totalTime){
+    console.log("total Times up...");
+  }
+
   second++;
+  partSecond++;
   timer = setTimeout(function () {
-    console.log("----Countdown----");
+    console.log("----Countdown----",partSecond,partTime);
     that.setData({
-      brewPercent: second / countSecond * 100,
+      partPercent: partSecond / partTime * 100,
       second: second,
+      partSecond:partSecond,
     });
     Countdown(that);
   }, 1000);
